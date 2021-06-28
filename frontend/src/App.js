38055140a727ec8143780879937266cc496e2e61
engrmazharjan <mazharj4565gmail.com>
2021-06-28 11:06:12 +0500

    React Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Pro Shop</h1>
-    </div>
-  )
+function App() {
+  return <h1>ProShop</h1>;
 }
 
-export default App
+export default App;
