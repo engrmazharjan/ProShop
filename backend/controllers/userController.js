34fc63f0f7fd0559618f4e3e4f582fa3c9ca9cfb
engrmazharjan <mazharj4565gmail.com>
2021-07-07 13:18:23 +0500

    Custom Authentication Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,4 +24,23 @@ const authUser = AsyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// @desc GET user profile
+// @route GET /api/users/profile
+// @access Private
+const getUserProfile = AsyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
+export { authUser, getUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
-import { authUser } from "../controllers/userController.js";
+import { authUser, getUserProfile } from "../controllers/userController.js";
+
 import express from "express";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // Routes
 router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
