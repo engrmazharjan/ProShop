9964ee39c455386fb753e3285366d2d2ae3c64f4
engrmazharjan <mazharj4565gmail.com>
2021-07-13 16:01:39 +0500

    Admin Product List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import LoginScreen from "./screens/LoginScreen";
 import OrderScreen from "./screens/OrderScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import ProductListScreen from "./screens/ProductListScreen";
 import ProductScreen from "./screens/ProductScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import React from "react";
@@ -34,6 +35,7 @@ function App() {
           <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/admin/userlist" component={UserListScreen} />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
+          <Route path="/admin/productlist" component={ProductListScreen} />
           <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
