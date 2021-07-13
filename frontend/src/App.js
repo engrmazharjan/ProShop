4837644a4b10694b8ad468a63d44710840e6594a
engrmazharjan <mazharj4565gmail.com>
2021-07-13 12:00:25 +0500

    Admin User List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import ProfileScreen from "./screens/ProfileScreen";
 import React from "react";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import UserListScreen from "./screens/UserListScreen";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           <Route path="/profile" component={ProfileScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/admin/userlist" component={UserListScreen} />
           <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
