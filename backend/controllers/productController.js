8e47533ae28326b8b1ee4cf53443d8c73057ccd5
engrmazharjan <mazharj4565gmail.com>
2021-07-15 15:33:09 +0500

    Product Pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,6 +5,8 @@ import Product from "../models/productModel.js";
 // @route GET /api/products
 // @access Public
 const getProducts = AsyncHandler(async (req, res) => {
+  const pageSize = 10;
+  const page = Number(req.query.pageNumber) || 1;
   const keyword = req.query.keyword
     ? {
         name: {
@@ -13,9 +15,12 @@ const getProducts = AsyncHandler(async (req, res) => {
         },
       }
     : {};
-  const products = await Product.find({ ...keyword });
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
 
-  res.json(products);
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 // @desc Fetch Single product
