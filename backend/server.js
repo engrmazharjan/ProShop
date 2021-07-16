7c6a5e3b52652ab89513008614bfc60781e1675e
engrmazharjan <mazharj4565gmail.com>
2021-07-16 14:54:23 +0500

    Prepare For Deployment

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,6 @@ if (process.env.NODE_ENV === "development") {
 // JSON
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API Is Running");
-});
-
 // Routes
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -44,6 +40,17 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("<h1>API Is Running...</h1>");
+  });
+}
 
 // Middleware
 app.use(notFound);
